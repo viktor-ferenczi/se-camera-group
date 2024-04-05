4e7e5524fc98de2d09caafb8137d7fa294cf187c
Viktor Ferenczi <viktor@ferenczi.eu>
2024-04-06 00:05:57 +0200

    Camera selection logic

--- ClientPlugin/ActivatePatch.cs
+++ ClientPlugin/ActivatePatch.cs
@@ -4,8 +4,12 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text;
 using HarmonyLib;
 using Sandbox.Game.Entities.Cube;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Interfaces;
 using VRage.Utils;
 
 namespace ClientPlugin
@@ -57,17 +61,60 @@ namespace ClientPlugin
 
             // Inject a static method call instead
             var handleCameraGroupMethodInfo = AccessTools.DeclaredMethod(typeof(ActivatePatch), nameof(HandleCameraGroup));
+            il.Insert(i++, new CodeInstruction(OpCodes.Ldloc_2));
             il.Insert(i, new CodeInstruction(OpCodes.Call, handleCameraGroupMethodInfo));
 
             il.RecordPatchedCode();
             return il;
         }
 
-        public static void HandleCameraGroup(List<MyTerminalBlock> terminalBlocks)
+        public static void HandleCameraGroup(List<MyTerminalBlock> terminalBlocks, ITerminalAction action)
         {
-            throw new Exception("HELL");
-            MyLog.Default.Info($"{Plugin.Name}: HandleCameraGroup()");
-            // return true;
+            // All cameras?
+            if (terminalBlocks.Count != 0 && terminalBlocks.All(block => block is IMyCameraBlock))
+            {
+                SelectNextCamera(terminalBlocks, action);
+                return;
+            }
+
+            // Original implementation from MyToolbarItemTerminalGroup.Activate
+            foreach (var block in terminalBlocks)
+            {
+                if (block != null && block.IsFunctional)
+                    action.Apply(block);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void SelectNextCamera(List<MyTerminalBlock> cameras, ITerminalAction action)
+        {
+            // Sort the cameras by their name which the player can change
+            cameras.Sort((a, b) => a.CustomName.CompareTo(b.CustomName));
+
+            // Camera system
+            var cameraSystem = cameras[0].CubeGrid.GridSystems.CameraSystem;
+
+            // The currently selected camera block or null if no camera is active
+            var currentCamera = cameraSystem.CurrentCamera;
+            var nextCameraIndex = currentCamera == null ? 0 : cameras.FindIndex(c => c.EntityId == currentCamera.EntityId) + 1;
+
+            // Select the next working camera
+            for (var i = 0; i < cameras.Count; i++)
+            {
+                nextCameraIndex %= cameras.Count;
+                var camera = cameras[nextCameraIndex];
+
+                if (camera.IsWorking)
+                {
+                    action.Apply(camera);
+                    if (cameraSystem.CurrentCamera == camera)
+                    {
+                        break;
+                    }
+                }
+
+                nextCameraIndex++;
+            }
         }
     }
 }
